57f31f63b947ccb97fc30df856510bc9eb978c44
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-04-25 04:22:28 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
 
+"""Exports to-do list information for a given employee ID to CSV format."""
+
 import csv
 import requests
 import sys
 
-
-def to_csv():
-    try:
-        user_id = int(sys.argv[1])
-    except IndexError:
-        print("Please provide a user ID as an argument.")
-        return
-
-    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
-    user = next((u for u in users if u.get('id') == user_id), None)
-    if user is None:
-        print("User not found.")
-        return
-    username = user.get('username')
-
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos").json()
-    task_status_title = [(t.get('completed'), t.get('title')) for t in todos if t.get('userId') == user_id]
-
-    filename = f"{user_id}.csv"
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-        writer.writeheader()
-        for task in task_status_title:
-            writer.writerow({"USER_ID": user_id, "USERNAME": username,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
-
-
 if __name__ == "__main__":
-    to_csv()
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
